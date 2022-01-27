94272c045febc438e8d854a423201cc20b1f0004
uslumt <81621143+uslumt@users.noreply.github.com>
2022-01-27 23:25:59 +0100

    Update Numerical_stability.m

--- Numerical_stability.m
+++ Numerical_stability.m
@@ -1,24 +1,24 @@
-%The idea behind this example is the least squares fitting of the function
-%exp(sin(4t)) on the interval [O, 1] by a polynomial of degree 14
-%we are going to compare just the coefficients x(15) computed by our various algorithms
+% The idea behind this example is the least squares fitting of the function
+% exp(sin(4t)) on the interval [O, 1] by a polynomial of degree 14
+% We are going to compare just the coefficients x(15) computed by our various algorithms
+
 m = 100; n = 15;
-t = (0 : m - 1)' / (m - 1);  %Set t to a discretization of [0, 1]
+t = (0 : m - 1)' / (m - 1);         %Set t to a discretization of [0, 1]
 A = []; 
 for i = 1 : n
-A = [A t.^(i-1)];            %Construct Vandermonde matrix
+A = [A t.^(i-1)];                   %Construct Vandermonde matrix
 end
 
 b = exp(sin(4 * t));
-b = b / 2006.787453080206;   %Normalization
-format long
+
 x=A\b;
-fprintf('x(15)=\n')
-%this figure was obtained with an extended precision arithmetic package
-x(15)
+fprintf('x(15) = %.15f \n', x(15))  %this figure was obtained with an extended precision arithmetic package
+
+b = b / 2006.787453080206;          %Normalization
+
 [Q , R] = qr(A , 0);
 x = R \ (Q' * b); 
-fprintf('QR-Householder \n')
-x(15)
+fprintf('QR-Householder = %.15f \n', x(15))
 
 [m , n] = size(A);
     V = A;
@@ -36,22 +36,16 @@ x(15)
 Q = V;
 
 x1 = R \ (Q' * b);
-fprintf('QR-Gram-Schmidt \n')
-x1(15)
-
+fprintf('QR-Gram-Schmidt = %.15f \n', x1(15))
 
 x2 = (A' * A) \ (A' * b) ;
-fprintf('Cholesky factorization \n')
-x2(15)
-
+fprintf('Cholesky factorization = %.15f \n', x2(15))
 
 [U, S, V] = svd(A); 
 x3 = V * (S \ (U' * b));
-fprintf('Singular value decomposition \n')
-x3(15)
+fprintf('Singular value decomposition = %.15f \n', x3(15))
 
 [L, U, P] = lu(A);
 y = L \ (P * b);
 x4 = U \ y;
-fprintf('LU with pivoting decomposition \n')
-x4(15)
+fprintf('LU with pivoting decomposition = %.15f \n', x4(15))
