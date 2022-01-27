c1cd405c0c034b5f7f7118a147911321c8b6ef4f
Alex <62043806+AlexMao0731@users.noreply.github.com>
2022-01-27 23:04:05 +0100

    Update Numerical_stability.m

--- Numerical_stability.m
+++ Numerical_stability.m
@@ -1,15 +1,20 @@
-
+%The idea behind this example is the least squares fitting of the function
+%exp(sin(4t)) on the interval [O, 1] by a polynomial of degree 14
+%we are going to compare just the coefficients x(15) computed by our various algorithms
 m = 100; n = 15;
-t = (0 : m - 1)' / (m - 1);
+t = (0 : m - 1)' / (m - 1);  %Set t to a discretization of [0, 1]
 A = []; 
 for i = 1 : n
-A = [A t.^(i-1)]; 
+A = [A t.^(i-1)];            %Construct Vandermonde matrix
 end
 
 b = exp(sin(4 * t));
-b = b / 2006.787453080206; 
+b = b / 2006.787453080206;   %Normalization
 format long
-
+x=A\b;
+fprintf('x(15)=')
+%this figure was obtained with an extended precision arithmetic package
+x(15)
 [Q , R] = qr(A , 0);
 x = R \ (Q' * b); 
 fprintf('QR-Householder \n')
